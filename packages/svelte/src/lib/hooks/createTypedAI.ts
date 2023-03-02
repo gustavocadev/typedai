70a60228be02e9984688b03303aac8fc15bddd38
gustavocadev <churaalaygustavo11@gmail.com>
2023-03-02 08:50:26 -0500

    fix: rm logs and add generics

--- packages/svelte/src/lib/hooks/createTypedAI.ts
+++ packages/svelte/src/lib/hooks/createTypedAI.ts
@@ -1,12 +1,12 @@
 import { writable } from 'svelte/store';
 import { SSE } from 'sse.js';
 
-export const createTypedAI = (url: string) => {
+export function createTypedAI<T>(url: string) {
 	// a state for the real time typeAI
 	const realTimeTypeAI = writable('');
 
 	// a state for the input text
-	const userInput = writable('');
+	const userInput = writable<T>();
 
 	// error state
 	const isError = writable(false);
@@ -23,7 +23,7 @@ export const createTypedAI = (url: string) => {
 		payload: ''
 	});
 
-	const setOptionsPayload = (payload: string) => {
+	const setOptionsPayload = (payload: T) => {
 		options.update((c) => {
 			c.payload = JSON.stringify(payload);
 			return c;
@@ -31,7 +31,6 @@ export const createTypedAI = (url: string) => {
 	};
 
 	userInput.subscribe((value) => {
-		console.log({ value });
 		setOptionsPayload(value);
 	});
 
@@ -47,44 +46,41 @@ export const createTypedAI = (url: string) => {
 			if (e.data === '[DONE]') return;
 
 			const completionResponse = JSON.parse(e.data);
-			console.log(completionResponse, 'completionResponse');
 			// we only need the first choice
 			const [{ text }] = completionResponse.choices;
-			console.log(text, 'Text');
 
 			// update the realTimeTypeAI state
 			realTimeTypeAI?.update((c) => {
-				console.log(c, text, 'test');
 				return c + text;
 			});
 		} catch (err) {
 			isError?.set(true);
 			isLoading?.set(false);
 			console.error(err);
-			alert('Something went wrong!');
 		}
 	};
 
 	const errorHandler = () => {
-		console.log('error');
+		console.log(`Error: ${url}`);
 		isError?.set(true);
 		isLoading?.set(false);
 	};
 
-	const startTypeAI = () => {
+	const startTypedAI = () => {
 		const sse = new SSE(url, opts);
 		sse.addEventListener('error', errorHandler);
 		sse.addEventListener('message', messageHandler);
 		sse.stream();
 	};
 
-	const setUserInput = (value: unknown) => {
-		userInput.set(JSON.stringify(value));
+	// set the user input
+	const setUserInput = (value: T) => {
+		userInput.set(value);
 	};
 
 	return {
 		realTimeTypeAI,
-		startTypeAI,
+		startTypedAI,
 		setUserInput
 	};
-};
+}
